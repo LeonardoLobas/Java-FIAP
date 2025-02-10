81cf91d3306b110bd2619c44ae5431f8a4f2b8b5
LeonardoLobas <leonardolobas2703@gmail.com>
2025-02-10 10:53:46 -0300

    Adicionar classe Olá com método main que imprime mensagem e loop de 1 a 5

---
+++ Primeiro-programa-java/Olá.java
@@ -0,0 +1,10 @@
+public class Olá {
+
+    public static void main(String[] args) {
+        System.out.println("Olá Alunos FIAP");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
