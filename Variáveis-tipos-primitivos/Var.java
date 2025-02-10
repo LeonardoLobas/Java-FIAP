1ac53885434266747fbf419e08d89713d79f3d22
LeonardoLobas <leonardolobas2703@gmail.com>
2025-02-10 11:51:41 -0300

    Adicionar classe Var com exemplo de tipos primitivos em Java

---
+++ Variáveis-tipos-primitivos/Var.java
@@ -0,0 +1,29 @@
+public class Var {
+        public static void main(String[] args) {
+           
+            //Tipos primitivos: byte,short,int,long
+            int idade;
+            idade = 22;
+
+            //float, double
+            double altura = 1.80;
+
+            //char
+            char genero = 'm';
+
+            //boolean
+            boolean especial = false;
+
+            //Tipo refência: String
+            String nome = "Leonardo";
+
+    
+            System.out.println("Idade: " + idade);
+            System.out.println("Altura: " + altura);
+            System.out.println("Gênero: " + genero);
+            System.out.println("Especial: " + especial);
+            System.out.println("Nome: " + nome);
+
+        }
+    
+}
