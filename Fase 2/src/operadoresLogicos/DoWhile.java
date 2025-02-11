782e561d8ada851127704e470658a64235a09b99
LeonardoLobas <leonardolobas2703@gmail.com>
2025-02-11 11:51:41 -0300

    For Loop

---
+++ Fase 2/src/operadoresLogicos/DoWhile.java
@@ -0,0 +1,32 @@
+package operadoresLogicos;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bem-vindo!");
+
+        Random random = new Random();
+        int numeroSecreto = random.nextInt(10) + 1;
+        int tentativas = 0;
+        int palpite;
+
+        System.out.println("Tente adivinhar o número entre 1 a 10.");
+
+        do {
+            System.out.println("Digite o seu palpite: ");
+            palpite = scanner.nextInt();
+            tentativas++;
+
+            if (palpite == numeroSecreto) {
+                System.out.println("Você acertou, o numero era: " + numeroSecreto);
+            }else {
+                System.out.println("Tente novamente!");
+            }
+        } while (palpite != numeroSecreto);
+        System.out.println("Você acertou em " + tentativas + " tentativas");
+        scanner.close();
+    }
+}
---
+++ Fase 2/src/operadoresLogicos/ForLoop.java
@@ -0,0 +1,15 @@
+package operadoresLogicos;
+public class ForLoop {
+        public static void main(String[] args) {
+            System.out.println("Tabela de Multiplicação:");
+
+            for (int i = 1; i <= 10; i++) {
+                System.out.println("\nTabuada do " + i + ":");
+
+                for (int j = 1; j <= 10; j++) {
+                    int resultado = i * j;
+                    System.out.println(i + " x " + j + " = " + resultado);
+                }
+            }
+        }
+}
