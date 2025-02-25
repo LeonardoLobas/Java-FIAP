279ec5ffa2f6e5cafdddac139dc06afcd39c2749
LeonardoLobas <leonardolobas2703@gmail.com>
2025-02-25 10:16:13 -0300

    Orientação objetos.

---
+++ Fase 5/src/OrientaçãoObejtos/ItemPersonagem.java
@@ -0,0 +1,16 @@
+package OrientaçãoObejtos;
+
+public class ItemPersonagem {
+    String nome;
+    String descricao;
+    boolean raro;
+    int nivelPoder;
+
+    public ItemPersonagem(String nome,String descricao,boolean raro,int nivelPoder) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.raro = raro;
+        this.nivelPoder = nivelPoder;
+    }
+}
+
