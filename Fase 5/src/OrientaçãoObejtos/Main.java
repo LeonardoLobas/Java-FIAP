820be3528f8bde5ab06d148ef5b6f228c3d5ba31
LeonardoLobas <leonardolobas2703@gmail.com>
2025-02-25 08:25:25 -0300

    Menu orientação...

--- Fase 5/src/OrientaçãoObejtos/Main.java
+++ Fase 5/src/OrientaçãoObejtos/Main.java
@@ -4,21 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Criando um objeto do tipo Personagem Mágico
-        PersonagemMagico mago = new PersonagemMagico();
+        //Criando um objeto do tipo Personagem Mágico mago = instancia new = estou instanciando
+        PersonagemMagico mago = new PersonagemMagico( "Gandalf", 100,"Magia");
 
-        //Atribuindo valores aos atributos do objeto
-        mago.nome = "Gandalf";
-        mago.nivelEnergia = 100;
-        mago.poderMagico = "Magia";
 
         //Exibindo valores dos atributos do objeto
         System.out.println(mago.nome + " Energia: " + mago.nivelEnergia + " Poder: " + mago.poderMagico);
 
-        PersonagemMagico elfo = new PersonagemMagico();
-        elfo.nome = "Legolas";
-        elfo.nivelEnergia = 50;
-        elfo.poderMagico = "Arqueira";
+        PersonagemMagico elfo = new PersonagemMagico( "Legolas",50,"Arqueira");
+
         System.out.println(elfo.nome + " Energia: " + elfo.nivelEnergia + " Poder: " + elfo.poderMagico);
     }
 }
--- Fase 5/src/OrientaçãoObejtos/PersonagemMagico.java
+++ Fase 5/src/OrientaçãoObejtos/PersonagemMagico.java
@@ -5,5 +5,12 @@ public class PersonagemMagico {
     String nome;
     int nivelEnergia;
     String poderMagico;
+// como o objeto sera instanciado ou criado
+    public PersonagemMagico(String nome, int nivelEnergia,String poderMagico) {
+        this.nome = nome;
+        this.nivelEnergia = nivelEnergia;
+        this.poderMagico = poderMagico;
+    }
 
+    public PersonagemMagico(){}
 }
