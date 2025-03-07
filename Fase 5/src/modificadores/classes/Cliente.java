4bd0e2794caf4c2927e3842faec9f72c859439e2
LeonardoLobas <leonardolobas2703@gmail.com>
2025-03-07 17:48:03 -0300

    Criando classes testes

---
+++ Fase 5/src/modificadores/classes/Cliente.java
@@ -0,0 +1,13 @@
+package modificadores.classes;
+
+public class Cliente {
+
+    private String nome;
+
+    private String cpf;
+
+    void exibirDados() {
+        System.out.println(nome + " " + cpf);
+    }
+
+}
---
+++ Fase 5/src/modificadores/classes/TestesCodigo.java
@@ -0,0 +1,8 @@
+package modificadores.classes;
+
+public class TestesCodigo {
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente();
+
+    }
+}
